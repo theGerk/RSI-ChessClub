a11f8dc8648c869e7e18a2b4706541f0516b3e2d
Benji Altman <Benji@altmansoftwaredesign.com>
2019-10-08 19:50:53 -0500

    wrote SA algorithm

--- SimulatedAnnealing.ts
+++ SimulatedAnnealing.ts
@@ -1,7 +1,14 @@
 ﻿namespace SimulatedAnnealing
 {
-	export function solve<T>(initialState: T, costFunction: (state: T) => number, getRandomNeighboor: (state: T) => T, moveProbability: (currentState: T, candidateState: T, tempature: number) => number, steps: number): T
+	export function solve<T>(initialState: T, costFunction: (state: T) => number, getRandomNeighboor: (state: T) => T, moveProbability: (currentStateCost: number, candidateStateCost: number, tempature: number) => number, steps: number): T
 	{
-
+		let currentState = initialState;
+		for(let k = 1; k <= steps; k++)
+		{
+			let candidate = getRandomNeighboor(currentState);
+			if(moveProbability(costFunction(currentState), costFunction(candidate), steps / k) >= Math.random())
+				currentState = candidate;
+		}
+		return currentState;
 	}
 }
