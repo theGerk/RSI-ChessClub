e9d5e65d50bd14876215afadc9e78fa6dfee7b40
Benji Altman <Benji@altmansoftwaredesign.com>
2019-10-08 19:18:44 -0500

    add simulated annealing file, namespace, and general function's signature

---
+++ SimulatedAnnealing.ts
@@ -0,0 +1,7 @@
+﻿namespace SimulatedAnnealing
+{
+	export function solve<T>(initialState: T, costFunction: (state: T) => number, getRandomNeighboor: (state: T) => T, moveProbability: (currentState: T, candidateState: T, tempature: number) => number, steps: number): T
+	{
+
+	}
+}
