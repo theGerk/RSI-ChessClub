ed7499552ae189bf04aac2a129ba628ed524d8f3
Benji Altman <Benji@altmansoftwaredesign.com>
2019-10-10 18:19:19 -0500

    ???

--- Dev.ts
+++ Dev.ts
@@ -4,6 +4,6 @@ function test()
 	let players = FrontEnd.Master.getActivePlayersArray();
 	for(let i = 0; i < 100; i++)
 	{
-		Logger.log(Pairings.totalCost(Pairings.pair(players, true)));
+		Logger.log(Pairings.totalCost(Pairings.pair(players)));
 	}
 }
