d03645ead71c6e8739096d0421104a8380696d6a
Benji <benji@altmansoftwaredesign.com>
2019-10-10 14:27:28 -0500

    done a lot of work to test ai functions

--- Dev.ts
+++ Dev.ts
@@ -1,9 +1,62 @@
 ﻿
 function test()
 {
-	let players = FrontEnd.Master.getActivePlayersArray();
-	for(let i = 0; i < 100; i++)
+	let clubArray = FrontEnd.Master.getActivePlayersArray();
+	let playerCount = clubArray.length;
+	let clubMap = Benji.makeMap(clubArray, (p) => p.name);
+	for(let i = 0; i < 20; i++)
 	{
-		Logger.log(Pairings.totalCost(Pairings.pair(players, true)));
+		let pairings = Pairings.pair(clubArray);
+
+		let gamesResults: { Tournament: FrontEnd.Games.IGame[], Other: FrontEnd.Games.IGame[] } = { Tournament: [], Other: [] }
+
+		//put in tournament games
+		for(let j = 0; j < pairings.length; j++)
+		{
+			let random = Math.random();
+			if(random < .2)
+				continue;
+			else if(random <= .575)
+				gamesResults.Tournament.push({ white: pairings[i].white.name, black: pairings[i].black.name, result: 1 });
+			else if(random <= .95)
+				gamesResults.Tournament.push({ white: pairings[i].white.name, black: pairings[i].black.name, result: 0 });
+			else
+				gamesResults.Tournament.push({ white: pairings[i].white.name, black: pairings[i].black.name, result: .5 });
+		}
+
+		while(Math.random() < .92)
+		{
+			let index1 = Math.floor(Math.random() * playerCount);
+			let index2 = Math.floor(Math.random() * (playerCount - 1));
+			if(index2 >= index1)
+				index2++;
+			let result: number;
+			let resultGenerator = Math.random();
+			if(resultGenerator < .47)
+				result = 0;
+			else if(resultGenerator < .94)
+				result = 1;
+			else
+				result = .5;
+
+			gamesResults.Other.push({white:clubArray[index1].name, black:clubArray[index2].name, result: result})
+		}
+
+		//do ratings
+		let everyoneRatings: Glicko.IRating[] = [];
+		for(let index in clubMap)
+			everyoneRatings.push(clubMap[index].rating);
+
+		Glicko.doRatingPeriod(index => clubMap[index].rating, gamesResults.Tournament.concat(gamesResults.Other), everyoneRatings);
+
+
+		//add games to history
+		let tourny = gamesResults.Tournament;
+		for(let i = tourny.length - 1; i >= 0; i--)
+		{
+			let currentGame = tourny[i];
+			clubMap[currentGame.white].pairingHistory.push({ opponent: currentGame.black, white: true });
+			clubMap[currentGame.black].pairingHistory.push({ opponent: currentGame.white, white: false });
+		}
 	}
 }
--- Pairings.ts
+++ Pairings.ts
@@ -3,6 +3,41 @@
 
 namespace Pairings
 {
+	export namespace Testing
+	{
+		export interface ITestReturn
+		{
+			runs: number;
+			totalTime: number;
+			costs: number[];
+		}
+
+
+		export function ComparePairingMethods(input: IPlayer[], runs: number = 1000): { [testName: string]: ITestReturn }
+		{
+			input = [...input];
+			if(input.length % 2 !== 0)
+				input.push(null);
+			return {
+				"Stupid Greedy (first used method)": doTest(input, stupidGreedy, runs),
+				"Pure Hill climb": doTest(input, hillClimb, runs),
+				"Pure Random": doTest(input, randomPairing, runs),
+			};
+		}
+
+		function doTest(players: IPlayer[], func: (input: IPlayer[]) => IPairing[], runs: number): ITestReturn
+		{
+			let costs: number[] = [];
+			let start = Date.now();
+			for(let i = 0; i < runs; i++)
+				costs.push(totalCost(func(players)));
+			let end = Date.now();
+			return { runs: runs, totalTime: end - start, costs: costs };
+		}
+	}
+
+
+
 	export interface IPairing
 	{
 		white: IPlayer;
@@ -47,11 +82,18 @@ namespace Pairings
 	 * @param dontCopy set to truthy value only if the ordering in players array does not mater.
 	 * @returns an array of pairings
 	 */
-	export function pair(players: IPlayer[], dontCopy?: boolean)
+	export function pair(players: IPlayer[]): IPairing[]
 	{
-		if(!dontCopy)
-			players = [...players];
+		players = [...players];
 
+		if(players.length % 2 !== 0)
+			players.push(null);
+
+		return assignColor(hillClimb(players));
+	}
+
+	function stupidGreedy(players: IPlayer[]): IPairing[]
+	{
 		//shuffle for randomness
 		Benji.shuffle(players);
 
@@ -72,29 +114,85 @@ namespace Pairings
 			//get costs array
 			let costs = getCosts(player, players.filter(p => !usedSet.hasOwnProperty(p.name)));
 
-			if(costs.length === 0)
-				break;
-
 			let opponent = costs[0].player;
 			usedSet[opponent.name] = true;
 
-			//assign black and white based on who has a lower ratio of black to white, otherwise simply do random
-			let playerRatio = whiteFraction(player.pairingHistory);
-			let opponentRatio = whiteFraction(opponent.pairingHistory);
-
-			//do the push
-			if(playerRatio < opponentRatio)
-				output.push({ white: player, black: opponent });
-			else if(playerRatio > opponentRatio)
-				output.push({ black: player, white: opponent });
-			else
-				if(Math.random() > .5)
-					output.push({ white: player, black: opponent });
-				else
-					output.push({ black: player, white: opponent });
+			output.push({ white: player, black: opponent });
 		}
 		return output;
 	}
+
+	function hillClimb(players: IPlayer[]): IPairing[]
+	{
+		let state = randomPairing(players);
+		let cost = totalCost(state);
+
+		while(true)
+		{
+			let newState = getBestNeighboor(state);
+			let newCost = totalCost(newState);
+
+
+			if(cost <= newCost)
+			{
+				return state;
+			}
+			else
+			{
+				state = newState;
+				cost = newCost;
+			}
+		}
+	}
+
+
+	//function simulatedAnnealing(players: IPlayer[]): IPairing[]
+	//{
+
+	//}
+
+	function randomPairing(players: IPlayer[]): IPairing[]
+	{
+		Benji.shuffle(players);
+
+		let output: IPairing[] = [];
+
+		for(let i = players.length - 1; i >= 0; i -= 2)
+			output.push({ white: players[i], black: players[i - 1] });
+
+		return output;
+	}
+
+
+	/**
+	 * Assigns colors to both an already created pairings
+	 * @param pairing
+	 */
+	function assignColor(pairing: IPairing[])
+	{
+		for(let i = pairing.length - 1; i >= 0; i--)
+		{
+			let pair = pairing[i];
+			let white = pair.white;
+			let black = pair.black;
+
+			if(white === null || black === null)
+				continue;
+			
+			//find how often each player is white
+			let whiteRatio = whiteFraction(white.pairingHistory);
+			let blackRatio = whiteFraction(black.pairingHistory);
+
+			//if white has been white more often then switch it, otherwise do nothing
+			if(whiteRatio > blackRatio)
+			{
+				pair.white = black;
+				pair.black = white;
+			}
+		}
+		return pairing;
+	}
+
 
 	/**
 	 * Gets sorted costs of each game
@@ -143,7 +241,8 @@ namespace Pairings
 
 		for(let i = 1; i < length; i++)
 			for(let j = 0; j < i; j++)
-				for(let colorIndex = 0; colorIndex < 2; colorIndex++)	//only two possible colors, white and black in chess. Should not be a problem to hardcode this 2.
+				//Only two possible colors, white and black in chess. Should not be a problem to hard-code this two.
+				for(let colorIndex = 0; colorIndex < 2; colorIndex++)
 				{
 					let current = [...input];
 
@@ -151,10 +250,21 @@ namespace Pairings
 					let jPair = current[j] = duplicatePairing(current[j]);
 
 					let color = _colors[colorIndex];
+
+					//swap between iPair.white and jPair[color].
 					let tmp = iPair.white;
-					iPair.white = jPair[_colors[colorIndex]]
-					jPair[_colors[colorIndex]]
+					iPair.white = jPair[color]
+					jPair[color] = tmp;
+
+					let cost = totalCost(input);
+					if(cost < bestCost)
+					{
+						bestCost = cost;
+						best = current;
+					}
 				}
+
+		return best;
 	}
 
 	/**
