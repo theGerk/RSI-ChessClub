eb121820b8afff91bd82c08b7856443d5d60eea6
Benji Altman <Benji@altmansoftwaredesign.com>
2019-10-10 19:34:19 -0500

    reading JSON now

--- localTesting/app.ts
+++ localTesting/app.ts
@@ -1,2 +1,9 @@
-﻿let club = require('club.json');
-console.log(JSON.stringify(club));
+﻿///<reference path="Constants.ts"/>
+///<reference path="Pairings.ts"/>
+
+let fs = require('fs');
+let club = JSON.parse(fs.readFileSync('club.json', 'utf8'));
+
+for(let i = 0; i < 10; i++)
+	console.log('hello world');
+
